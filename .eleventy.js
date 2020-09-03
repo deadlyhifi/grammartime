4fd95cb08f275466802a446d09515d489aeb429b
Tom de Bruin <tomdebruin@gmail.com>
2020-09-03 19:46:09 +0100

    Excerpt

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site/css");
 
+  eleventyConfig.addFilter("excerpt", function (value) {
+    const content = value.template.frontMatter.content;
+
+    return content.length > 50 ? `${content.substring(0, 50)}…` : null;
+
+  });
+
   return {
     dir: {
       input: "site",
